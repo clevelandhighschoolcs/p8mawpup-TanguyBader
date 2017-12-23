114ad9c76c88c3ef727780fe0861e46fa1fe4574
Everett Kays <33072639+kz3838@users.noreply.github.com>
2017-12-23 12:53:35 -0800

    Update webmonitoring16.py

--- webmonitoring16.py
+++ webmonitoring16.py
@@ -38,27 +38,27 @@ def main ():
 			from_=twilio_phone_number
 		)
 		
-		#export to excel csv
+		export to excel csv
 		
-#		import csv
-#		with open('index.csv', 'a') as csv_file:
-#			writer = csv.writer(csv_file)
-#			writer.writerow([name, price, datetime.now()])
-#		quote_page = ['http://bobphysicsone.blogspot.com/', ‘http://bobphysicsone.blogspot.com/']
+		import csv
+		with open('index.csv', 'a') as csv_file:
+			writer = csv.writer(csv_file)
+			writer.writerow([name, price, datetime.now()])
+		quote_page = ['http://bobphysicsone.blogspot.com/', ‘http://bobphysicsone.blogspot.com/']
 		
-#		data = [] 
-#		for pg in quote_page:
-#		page = urllib2.urlopen(pg)
-#		soup = BeautifulSoup(page, 'html.parser')
-#		name_box = soup.find('li', attrs={'class': 'archivedate'})
-#		name = name_box.text.strip() 
-#		price_box = soup.find('div', attrs={'class':'widget-content'})
-#		price = price_box.text
-#		data.append((name, price))
-#		with open('index.csv', 'a') as csv_file:
-#			writer = csv.writer(csv_file)
-#		for name, price in data:
-#		writer.writerow([name, price, datetime.now()])
+		data = [] 
+		for pg in quote_page:
+		page = urllib2.urlopen(pg)
+		soup = BeautifulSoup(page, 'html.parser')
+		name_box = soup.find('li', attrs={'class': 'archivedate'})
+		name = name_box.text.strip() 
+		price_box = soup.find('div', attrs={'class':'widget-content'})
+		price = price_box.text
+		data.append((name, price))
+		with open('index.csv', 'a') as csv_file:
+			writer = csv.writer(csv_file)
+		for name, price in data:
+		writer.writerow([name, price, datetime.now()])
 		
 		
  
